6818549931aaa0bcd272671dd96dd7c45cb2092d
tunguyena7 <69301575+tunguyena7@users.noreply.github.com>
2021-10-09 23:42:28 +0700

    list product

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import logo from "./logo.svg";
-import "./App.css";
 import TodoFeature from "./features/Todo";
 import Count from "./features/Counter";
 import { useEffect } from "react";
 import productApi from "./api/productApi";
 import Header from "components/Header";
+import { Route, Switch } from "react-router";
+import ProductFeature from "features/Product";
 
 function App() {
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const products = await productApi.getAll({
-        _limit: 10,
-      });
-    };
-    fetchProducts();
-  }, []);
+  // useEffect(() => {
+  //   const fetchProducts = async () => {
+  //     const products = await productApi.getAll({
+  //       _limit: 10,
+  //     });
+  //   };
+  //   fetchProducts();
+  // }, []);
 
   return (
     <div className="App">
       <Header />
-      <TodoFeature />
-      <Count />
+
+      <Switch>
+        <Route path="/products" component={ProductFeature} />
+        <Route path="/" component={ProductFeature} />
+      </Switch>
+      {/* <TodoFeature />
+      <Count /> */}
     </div>
   );
 }
---
+++ src/constants/common.js
@@ -0,0 +1,3 @@
+export const STATIC_HOST = "https://api.ezfrontend.com";
+export const THUMBNAIL_NOTFOUND =
+  "https://image.freepik.com/free-photo/error-page-found-system-updates-system-maintenance-installation-programs-concept_73903-949.jpg";
